d85c2252043d22efb4436455e8a88226340ce1ea
AchrafLamsahel <achraflamsahel1@gmail.com>
2024-02-16 15:22:30 +0100

    first-Commit

---
+++ src/test/java/ma/arkToDoApp/ToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.arkToDoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
